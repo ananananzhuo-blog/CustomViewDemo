44040cff039d57c5d88ea4f2894b33ff02c60ee7
ymeddmn <1275054668@qq.com>
2021-06-07 11:05:00 +0800

    旋转画小圆圈

--- app/src/main/java/com/mage/customviewdemo/view/CanvasRotationView.kt
+++ app/src/main/java/com/mage/customviewdemo/view/CanvasRotationView.kt
@@ -1,8 +1,27 @@
 package com.mage.customviewdemo.view
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class CanvasRotationView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+    val paint = Paint().apply {
+        color= Color.RED
+        style=Paint.Style.STROKE
+        isAntiAlias=true
+    }
+    init {
+
+    }
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.translate(measuredWidth/2f,measuredHeight/2f)
+        for (i in 0..360 step 60){
+            canvas?.drawCircle(100f,0f,20f,paint)
+            canvas?.rotate(60f)
+        }
+    }
 }
